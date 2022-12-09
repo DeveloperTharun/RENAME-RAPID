6db47cd330a6cb1d0f7f9c4b7db7517d662fd673
DeveloperTharun <114932079+DeveloperTharun@users.noreply.github.com>
2022-12-09 10:41:47 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -34,7 +34,10 @@ from config import START_PIC, FLOOD, ADMIN
 @Client.on_message(filters.private & filters.create(not_subscribed))
 async def is_not_subscribed(client, message):
     buttons = [[ InlineKeyboardButton(text="🤖 Join Updates channel", url=client.invitelink) ]]
-    text ="Hey, You Have To Join @Rapid_Bots Channel To Get Access To This Bot 😉."
+    text ='''Hey, You Have To Join @Rapid_Bots Channel To Get Access To This Bot.😉
+
+Please Join👇 And Come Back Here...
+👉 Link: https://t.me/Rapid_Bots''',
     await message.reply_text(text=text, reply_markup=InlineKeyboardMarkup(buttons))
            
 @Client.on_message(filters.private & filters.command(["start"]))
